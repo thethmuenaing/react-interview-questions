535f192f5f19ef08da4550a7e404334186aeba58
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-05-15 16:00:19 +0630

    react state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Coding01 from "./comments/Coding01";
+import Coding02 from "./comments/Coding02";
 
 function App() {
 	return (
 		<div className="w-[75%] mx-auto mt-32">
-			<Coding01 />
+			<Coding02 />
 		</div>
 	);
 }
---
+++ src/comments/Coding02.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const Coding02 = () => {
+	const [count, setCount] = useState(0);
+	const [name, setName] = useState("");
+	const [isVisible, setIsVisible] = useState(false);
+	const [array, setArray] = useState([
+		{
+			name: "Nishant",
+			age: 26,
+		},
+		{
+			name: "Uday",
+			age: 26,
+		},
+	]);
+	const [object, setObject] = useState({
+		name: "Nishant",
+		age: 26,
+	});
+
+	const increment = () => {
+		setCount(count + 1);
+		setIsVisible(!isVisible);
+		setName("Nishant");
+		setArray([...array, { name: "Thet", age: 21 }]);
+		setObject("");
+	};
+	return (
+		<div>
+			<div className="text-center">
+				<h1>Coding02</h1>
+				<h1>What are states</h1>
+			</div>
+
+			{isVisible ? <h1>Visible</h1> : <h1>Not Visible</h1>}
+			<h1>{name}</h1>
+			<h1>
+				{array.map((name, index) => (
+					<div key={index}>
+						{name.name} - {name.age}
+					</div>
+				))}
+			</h1>
+			<h1>
+				{object.name} {object.age}
+			</h1>
+
+			<h1>{count}</h1>
+			<button
+				onClick={increment}
+				className="px-4 py-3 border rounded-md bg-gray-300"
+			>
+				Change States
+			</button>
+		</div>
+	);
+};
+
+export default Coding02;
