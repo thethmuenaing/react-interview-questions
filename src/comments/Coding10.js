bc61d3c8d32b8150b59d029e8fe7608fa26144dc
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-05-21 21:27:48 +0630

    knowledge for higher-order component

---
+++ src/comments/Coding10.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Coding10 = () => {
+	return (
+		<div>
+			<div>
+				<h1>Coding10</h1>
+				<h2>A higher-order component (HOC)</h2>
+				<h3>
+					A higher-order component (HOC) is an advanced technique in React for
+					reusing component logic. HOCs are not part of the React API, per se.
+					They are a pattern that emerges from React's compositional nature.
+					Concretely, a higher-order component is a function that takes a
+					component and returns a new component.
+				</h3>
+			</div>
+		</div>
+	);
+};
+
+export default Coding10;
